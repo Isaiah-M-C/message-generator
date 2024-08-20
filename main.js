a68ec64ee07988a51798ada6bbaae5f3732bf454
Isaiah-M-C <isaiahclark2@icloud.com>
2024-08-20 13:43:00 -0500

    add choose message function

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 const compliments = ['You look amazing today', 'You have nice eyes', 'I really like your outfit', 'have a good day'];
+const chooseMessage = () => {
+    return compliments[Math.floor(Math.random() * 4)];
+};
+
+console.log(chooseMessage);
